fd22c6dc745f23f50ec6682027e1debb5a952fc3
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2020-06-06 17:16:18 +0600

    terminal function created . It will set up a git project

---
+++ script_runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#print the input 
+function create() {
+    cd /home/hasnat/Documents/Projects/My\ Projects
+    mkdir $1
+    cd $1
+    touch README.md
+    git init
+    git remote add origin git@github.com:Hasnat79/$1.git 
+    git add .
+    git commit -m "initial commit"
+    git push -u origin master
+    code .
+}
